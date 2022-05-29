1c9e03f5cf06e91666075590d9ff18b6edaeb1fd
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-05-29 18:57:53 +0600

    Update `.env` file generation

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -e
 
-
-
 echo Generating .env file...
 
-echo SECRET_KEY="{{ cookiecutter.ENV_SECRET_KEY }}" > .env
+echo POSTGRES_PASSWORD="{{ cookiecutter.ENV_DB_PASSWORD }}" > .env
+echo POSTGRES_DB="{{ cookiecutter.ENV_DB_NAME }}" >> .env
+echo POSTGRES_USER="{{ cookiecutter.ENV_DB_USER }}" >> .env
+echo PGDATA=/var/lib/postgresql/data/pgdata >> .env
+
+echo SECRET_KEY="{{ cookiecutter.ENV_SECRET_KEY }}" >> .env
 echo DEBUG="{{ cookiecutter.ENV_DEBUG }}" >> .env
-echo DATABASE_URL="{{ cookiecutter.ENV_DATABASE_URL }}" >> .env
-echo DJANGO_STATIC_ROOT="{{ cookiecutter.ENV_DJANGO_STATIC_ROOT }}" >> .env
 echo ALLOWED_HOSTS="{{ cookiecutter.ENV_DJANGO_ALLOWED_HOSTS }}" >> .env
+echo DJANGO_STATIC_ROOT="{{ cookiecutter.ENV_DJANGO_STATIC_ROOT }}" >> .env
+echo DATABASE_URL="psql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@{{ cookiecutter.ENV_DB_HOST }}:5432/${POSTGRES_DB}" >> .env
 
 {% if cookiecutter.use_sentry == "yes" %}echo  SENTRY_DSN="{{ cookiecutter.ENV_SENTRY_DSN }}" >> .env {% endif %}
 
